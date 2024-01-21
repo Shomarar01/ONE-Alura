b9c218223c8ae441e5b5a6d3affb5ff3879e2443
Shomarar01 <157265226+Shomarar01@users.noreply.github.com>
2024-01-21 00:03:55 -0600

    Add files via upload
    Mis ejercicios de ONE

---
+++ app.js
@@ -0,0 +1,14 @@
+//let numeroSecreto = 1;
+//let numeroUsuario = prompt('Me indicas un número entre 1 y 10, por favor:');
+
+//console.log(numeroUsuario);
+
+/*Este código realiza una comparación */
+//if (numeroUsuario == numeroSecreto){
+    //Acertamos, fue verdadera la condición
+  //  alert(`Correcto acertaste, el número es: ${numeroUsuario}`);
+//} else {
+    //La condición no se cumplió
+   // alert('Lo siento, no acertaste el número');
+//}
+//console.log(numeroSecreto);
